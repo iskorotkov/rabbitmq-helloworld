449614f37367105cbf1ccfb20815264a262ce011
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-08-02 20:29:51 +0500

    Add support for several receivers

--- Receive/Receive.cs
+++ Receive/Receive.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
+using System.Threading;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
@@ -14,22 +16,32 @@ namespace Receive
             using var connection = factory.CreateConnection();
             using var channel = connection.CreateModel();
 
-            channel.QueueDeclare(queue: "hello",
-                 durable: false,
+            channel.QueueDeclare(queue: "task_queue",
+                 durable: true,
                  exclusive: false,
                  autoDelete: false,
                  arguments: null);
 
+            channel.BasicQos(0, 1, false);
+
+            Console.WriteLine("[x] Waiting for messages.");
+
             var consumer = new EventingBasicConsumer(channel);
-            consumer.Received += (model, ea) =>
+            consumer.Received += (sender, ea) =>
             {
                 var body = ea.Body.ToArray();
                 var message = Encoding.UTF8.GetString(body);
                 Console.WriteLine($"[x] Received {message}");
+
+                int dots = message.Count(c => c == '.');
+                Thread.Sleep(dots * 1000);
+                Console.WriteLine("[x] Done");
+
+                channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
             };
 
-            channel.BasicConsume(queue: "hello",
-                                 autoAck: true,
+            channel.BasicConsume(queue: "task_queue",
+                                 autoAck: false,
                                  consumer: consumer);
 
             Console.WriteLine("Press [enter] to exit.");
--- Send/Send.cs
+++ Send/Send.cs
@@ -1,36 +1,39 @@
-﻿using System;
-using System.Text;
+﻿using System.Text;
 using RabbitMQ.Client;
 
 namespace Send
 {
     internal static class Program
     {
-        private static void Main()
+        private static void Main(string[] args)
         {
             var factory = new ConnectionFactory() { HostName = "localhost" };
 
             using var connection = factory.CreateConnection();
             using var channel = connection.CreateModel();
 
-            channel.QueueDeclare(queue: "hello",
-                                 durable: false,
+            channel.BasicQos(0, 1, false);
+            channel.QueueDeclare(queue: "task_queue",
+                                 durable: true,
                                  exclusive: false,
                                  autoDelete: false,
                                  arguments: null);
 
-            const string message = "Hello World!";
+            var message = GetMessage(args);
             var body = Encoding.UTF8.GetBytes(message);
 
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+
             channel.BasicPublish(exchange: "",
-                                 routingKey: "hello",
-                                 basicProperties: null,
+                                 routingKey: "task_queue",
+                                 basicProperties: properties,
                                  body: body);
+        }
 
-            Console.WriteLine($"[x] Sent {message}");
-
-            Console.WriteLine("Press [enter] to exit.");
-            Console.ReadLine();
+        private static string GetMessage(string[] args)
+        {
+            return (args.Length > 0) ? string.Join(" ", args) : "Hello World!";
         }
     }
 }
