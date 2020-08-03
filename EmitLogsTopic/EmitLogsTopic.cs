6ad130cbe9e77c15056e46e3785a15e1e83d576b
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-08-03 15:58:35 +0500

    Add routing with topics

---
+++ EmitLogsTopic/EmitLogsTopic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Send
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "rabbitmq" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("topic_logs", ExchangeType.Topic);
+
+            var routingKey = (args.Length > 0) ? args[0] : "anonymous.info";
+            var message = (args.Length > 1) ? string.Join(" ", args.Skip(1).ToArray()) : "Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            Console.WriteLine($"[send] Sending message '{message}' with key '{routingKey}'.");
+            channel.BasicPublish(exchange: "topic_logs",
+                                 routingKey: routingKey,
+                                 basicProperties: null,
+                                 body: body);
+            Console.WriteLine($"[send] Message '{message}' sent with key '{routingKey}'.");
+        }
+    }
+}
---
+++ ReceiveLogsTopic/ReceiveLogsTopic.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Receive
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "rabbitmq" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("topic_logs", ExchangeType.Topic);
+
+            var queueName = channel.QueueDeclare().QueueName;
+
+            if (args.Length < 1)
+            {
+                Console.Error.WriteLine($"Usage: {Environment.GetCommandLineArgs()[0]}, [info] [warning] [error]");
+
+                Environment.ExitCode = 1;
+                return;
+            }
+
+            foreach (var routingKey in args)
+            {
+                channel.QueueBind(queue: queueName,
+                                  exchange: "topic_logs",
+                                  routingKey: routingKey);
+            }
+
+            Console.WriteLine("[receive] Waiting for messages.");
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                var routingKey = ea.RoutingKey;
+                Console.WriteLine($"[receive] Received message '{message}' with key '{routingKey}'");
+            };
+
+            channel.BasicConsume(queue: queueName,
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
