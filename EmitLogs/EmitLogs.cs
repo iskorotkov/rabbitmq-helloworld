f6b3d77e7c9194068f036fa71ed23898c4746df6
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-08-03 11:10:48 +0500

    Add projects to test and use exchanges

---
+++ EmitLogs/EmitLogs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Send
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "rabbitmq" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("logs", ExchangeType.Fanout);
+
+            var message = GetMessage(args);
+            var body = Encoding.UTF8.GetBytes(message);
+
+            Console.WriteLine($"[send] Sending message '{message}'.");
+            channel.BasicPublish(exchange: "logs",
+                                 routingKey: "",
+                                 basicProperties: null,
+                                 body: body);
+            Console.WriteLine($"[send] Message '{message}' sent.");
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return (args.Length > 0) ? string.Join(" ", args) : "Hello World!";
+        }
+    }
+}
---
+++ ReceiveLogs/ReceiveLogs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Receive
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "rabbitmq" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("logs", ExchangeType.Fanout);
+
+            var queueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(queue: queueName,
+                              exchange: "logs",
+                              routingKey: "");
+
+            Console.WriteLine("[receive] Waiting for messages.");
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"[receive] Received message '{message}'");
+            };
+
+            channel.BasicConsume(queue: queueName,
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
